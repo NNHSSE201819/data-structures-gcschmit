8b1040749400a62bb06956675d752e6ffbcb25ef
Geoff Schmit <gcschmit@gmail.com>
2018-09-06 15:18:24 -0500

    finished list notes

--- Chapter 15 Activities/Downsize/src/DownsizeTester.java
+++ Chapter 15 Activities/Downsize/src/DownsizeTester.java
@@ -1,23 +1,21 @@
-package Downsize;
-
 import java.util.LinkedList;
 
 /**
-   A test class for the downsize method.
-*/
+ * A test class for the downsize method.
+ */
 public class DownsizeTester
 {
-   public static void main(String[] args)
-   {
-      LinkedList<String> employeeNames = new LinkedList<>();
-      employeeNames.addLast("Dick");
-      employeeNames.addLast("Harry");
-      employeeNames.addLast("Romeo");
-      employeeNames.addLast("Tom");
+    public static void main(String[] args)
+    {
+        LinkedList<String> employeeNames = new LinkedList<>();
+        employeeNames.addLast("Dick");
+        employeeNames.addLast("Harry");
+        employeeNames.addLast("Romeo");
+        employeeNames.addLast("Tom");
 
-      Business.downsize(employeeNames, 2);
+        Business.downsize(employeeNames, 2);
 
-      System.out.println(employeeNames);
-      System.out.println("Expected: [Dick, Romeo]");
-   }
+        System.out.println(employeeNames);
+        System.out.println("Expected: [Dick, Romeo]");
+    }
 }
--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -2,21 +2,21 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 
 /**
-   This program demonstrates the LinkedList class
-	and ListIterator class.
-*/
+ * This program demonstrates the LinkedList class
+ * and ListIterator class.
+ */
 public class ListDemo
 {
-   public static void main(String[] args)
-   {
+    public static void main(String[] args)
+    {
         /*
             the addLast method of LinkedList can be used to populate a list
          */
         LinkedList<String> staff = new LinkedList<>();
-        staff.addLast( "Diana" );
-        staff.addLast( "Harry" );
-        staff.addLast( "Romeo" );
-        staff.addLast( "Tom" );
+        staff.addLast("Diana");
+        staff.addLast("Harry");
+        staff.addLast("Romeo");
+        staff.addLast("Tom");
 
         // the list is currently DHRT
 
@@ -25,29 +25,68 @@ public class ListDemo
                 at the front of the list
             The "|" in the comment indicates the iterator position
         */
-       ListIterator<String> iterator = staff.listIterator();    // |DHRT
+        ListIterator<String> iterator = staff.listIterator();    // |DHRT
 
+       /*
+            the next method advances the iterator over the next element in the list
+        */
+        iterator.next();     // D|HRT
 
-      // | in the comments indicates the iterator position
+       /*
+            the next method also returns the element that the iterator is passing
+        */
+        String name = iterator.next();   // DH|RT
+        System.out.println(name);
+        System.out.println("Expected: Harry");
 
-					// |DHRT
-       				// D|HRT
-      				// DH|RT
+       /*
+            the add method inserts an element at the iterator position
+                the iterator is then positioned after the element that was added
+        */
+        iterator.add("Juliet");      // DHJ|RT
+        iterator.add("Nina");        // DHJN|RT
 
-      // Add more elements after second element
+       /*
+            the remove method removes the element returned by the last call to
+                next or previous
+            the remove method can only be called once after calling next or
+                previous
+            the remove method cannot be called after calling add
+        */
+        iterator.next();     // DHJNR|T
+        iterator.remove();   // DHJN|T
 
-      				// DHJ|RT
-      				// DHJN|RT
+        System.out.println(staff);
+        System.out.println("Expected: [Diana, Harry, Juliet, Nina, Tom");
 
-      				// DHJNR|T
+       /* the set method updates the element returned by the last call to
+            next or previous
+        */
+        iterator.previous();     // DHJ|NT
+        iterator.set("Albert");  // DHJ|AT
 
-      // Remove last traversed element
+       /*
+            the hasNext method is often used in the context of a while loop
+        */
+        iterator = staff.listIterator();     // |DHJAT
+        while (iterator.hasNext())
+        {
+            String n = iterator.next();
+            if (n.equals("Juliet"))
+            {
+                iterator.remove();
+            }
+        }
 
-      				// DHJN|T
-
-      // Print all elements
-
-      //System.out.println(staff);
-      System.out.println("Expected: [Diana, Harry, Juliet, Nina, Tom]");
-   }
+       /*
+            enhanced for loops work with linked lists because they implement
+                the List interface
+        */
+        for (String n : staff)
+        {
+            System.out.println(n + " ");
+        }
+        System.out.println();
+        System.out.println("Expected Diana, Harry, Albert, Tom");
+    }
 }
