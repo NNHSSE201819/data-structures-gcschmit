23ef70fe6759a0e589e6a40ebd6987ed83355749
Geoff Schmit <gcschmit@gmail.com>
2018-09-11 14:29:12 -0500

    SpellCheck example for Sets

--- Chapter 15 Activities/Downsize/src/Business.java
+++ Chapter 15 Activities/Downsize/src/Business.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * Business utility methods.
@@ -13,6 +14,16 @@ public class Business
      */
     public static void downsize(LinkedList<String> employeeNames, int n)
     {
-        // ...
+        ListIterator<String> iter = employeeNames.listIterator();
+        int i = 0;
+        while (iter.hasNext())
+        {
+            iter.next();
+            i++;
+            if (i % n == 0)
+            {
+                iter.remove();
+            }
+        }
     }
 }
--- Chapter 15 Class Notes/src/SpellCheck.java
+++ Chapter 15 Class Notes/src/SpellCheck.java
@@ -1,46 +1,83 @@
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 /**
-   This program checks which words in a file are not present in a dictionary.
-*/
+ * This program checks which words in a file are not present in a dictionary.
+ */
 public class SpellCheck
 {
-   public static void main(String[] args)
-      throws FileNotFoundException
-   {
-      // Read the dictionary and the document
+    public static void main(String[] args)
+            throws FileNotFoundException
+    {
+        // Read the dictionary and the document
+        Set<String> dictionaryWords = readWords("src/words");
+        Set<String> documentWords= readWords("src/alice30.txt");
 
+        // Print all words that are in the document but not the dictionary
 
+        /*
+            the enhanced for loop works with sets
+                (so does Iterators with the hasNext method)
+         */
 
-      // Print all words that are in the document but not the dictionary
+        for (String word : documentWords)
+        {
+            if (!dictionaryWords.contains(word))
+            {
+                System.out.println(word);
+            }
+        }
 
-      //for (String word : documentWords)
-      {
-         //if ( )
-         {
-            //System.out.println(word);
-         }
-      }
-   }
+        System.out.println("unique words: " + documentWords.size());
 
-   /**
-      Reads all words from a file.
-      @param filename the name of the file
-      @return a set with all lowercased words in the file. Here, a
-      word is a sequence of upper- and lowercase letters.
-   */
-   public static Set<String> readWords(String filename)
-      throws FileNotFoundException
-   {
+        /*
+            Print the number of unique words inthe document with more than
+                three letters.
+         */
+        Iterator<String> i = documentWords.iterator();
+        while(i.hasNext())
+        {
+            if(i.next().length() < 4)
+            {
+                i.remove();
+            }
+        }
 
+        System.out.println("unique words (> 3 letters): " + documentWords.size());
+    }
 
+    /**
+     * Reads all words from a file.
+     *
+     * @param filename the name of the file
+     * @return a set with all lowercased words in the file. Here, a
+     * word is a sequence of upper- and lowercase letters.
+     */
+    public static Set<String> readWords(String filename)
+            throws FileNotFoundException
+    {
+        /* The implementation of the Set doesn't matter; so, store
+            the reference in a variable of type Set
+         */
+        Set<String> words = new HashSet<>();
+        Scanner in = new Scanner(new File(filename));
 
+        // use any character other than a-z or A-Z as delimiters
+        in.useDelimiter("[^a-zA-Z]+");
 
+        while(in.hasNext())
+        {
+            /*
+                adding duplicates to a set is ignored
+                    (so is removing elements that don't exist)
+             */
+            words.add(in.next().toLowerCase());
+        }
 
-      return null;
-   }
+        return words;
+    }
 }
