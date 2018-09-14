5e0b26ccb5f5f0ffa7a1c9c46f5c5a28df5f56a6
Geoff Schmit <gcschmit@gmail.com>
2018-09-14 14:44:34 -0500

    completed MapDemo class as an example of maps; formatted code in projects

--- Chapter 15 Activities/DistanceFinder/src/DistanceFinderDemo.java
+++ Chapter 15 Activities/DistanceFinder/src/DistanceFinderDemo.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
+
 public class DistanceFinderDemo
 {
-   public static void main(String[] args)
-   {
-      Scanner in = new Scanner(System.in);
-      System.out.print("File with city names: ");
-      String filename = in.nextLine();
-      DistanceFinder d = new DistanceFinder(filename);
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("File with city names: ");
+        String filename = in.nextLine();
+        DistanceFinder d = new DistanceFinder(filename);
 
-      System.out.println("Shortest distance from " + d.getStartingCity());
-      System.out.println(d.shortestDistances());
-   }
+        System.out.println("Shortest distance from " + d.getStartingCity());
+        System.out.println(d.shortestDistances());
+    }
 }
--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,49 +1,45 @@
 package Gradebook;
 
 import java.util.Scanner;
-. . .
+...
+
 /**
-   A program to add, remove, modify or print
-   student names and grades.
-*/
+ * A program to add, remove, modify or print
+ * student names and grades.
+ */
 public class Gradebook
 {
-   public static void main(String[] args)
-   {
-      Scanner in = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
 
       . . .
 
-      boolean done = false;
-      while(!done)
-      {
-         System.out.println("A)dd R)emove M)odify P)rint Q)uit");
-         String input = in.next().toUpperCase();
-         if (input.equals("Q"))
-         {
-            done = true;
-         }
-         else if (input.equals("A"))
-         {
+        boolean done = false;
+        while (!done)
+        {
+            System.out.println("A)dd R)emove M)odify P)rint Q)uit");
+            String input = in.next().toUpperCase();
+            if (input.equals("Q"))
+            {
+                done = true;
+            } else if (input.equals("A"))
+            {
             . . .
 
-         }
-         else if (input.equals("R"))
-         {
+            } else if (input.equals("R"))
+            {
             . . .
-         }
-         else if (input.equals("M"))
-         {
+            } else if (input.equals("M"))
+            {
             . . .
-         }
-         else if (input.equalsIgnoreCase("P"))
-         {
+            } else if (input.equalsIgnoreCase("P"))
+            {
             . . .
-         }
-         else
-         {
-            done = true;
-         }
-      }
-   }
+            } else
+            {
+                done = true;
+            }
+        }
+    }
 }
