85905e202f87b16b2f667f3e002cf2ae8b25223c
Geoff Schmit <gcschmit@gmail.com>
2018-09-27 15:11:29 -0500

    started LinkedList class

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -1,115 +1,127 @@
 import java.util.NoSuchElementException;
 
 /**
-   A linked list is a sequence of nodes with efficient
-   element insertion and removal. This class
-   contains a subset of the methods of the standard
-   java.util.LinkedList class.
-*/
+ * A linked list is a sequence of nodes with efficient
+ * element insertion and removal. This class
+ * contains a subset of the methods of the standard
+ * java.util.LinkedList class.
+ */
 public class LinkedList
 {
+    /*
+        first refers to the first node in this list
+            If the list is empty, first is null
+     */
+    private Node first;
+
+    /**
+     Constructs an empty linked list.
+     */
+    public LinkedList()
+    {
+        this.first = null;
+    }
 
 
-   /**
-      Constructs an empty linked list.
-   */
+    /**
+     Returns the first element in the linked list.
+     @return the first element in the linked list
+     */
+    public Object getFirst()
+    {
+        if(this.first == null)
+        {
+            throw new NoSuchElementException();
+        }
+
+        return this.first.data;
+    }
+
+    /**
+     Removes the first element in the linked list.
+     @return the removed element
+     */
+    public Object removeFirst()
+    {
+        if(this.first == null)
+        {
+            throw new NoSuchElementException();
+        }
+
+        Object element = this.first.data;
+        this.first = this.first.next;
+        return element;
+    }
 
 
+    /**
+     Adds an element to the front of the linked list.
+     @param element the element to add
+     */
+    public void addFirst(Object element)
+    {
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = this.first;
+        this.first = newNode;
+    }
 
 
-   /**
-      Returns the first element in the linked list.
-      @return the first element in the linked list
-   */
+    /**
+     * Returns an iterator for iterating through this list.
+     *
+     * @return an iterator for iterating through this list
+     */
 
 
+    // Class Node
+    class Node
+    {
+        public Object data;
+        public Node next;
+    }
+
+    class LinkedListIterator //implements ListIterator
+    {
+        //private data
 
 
-   /**
-      Removes the first element in the linked list.
-      @return the removed element
-   */
-
-
-
-
-
-   /**
-      Adds an element to the front of the linked list.
-      @param element the element to add
-   */
-
-
-
-
-
-   /**
-      Returns an iterator for iterating through this list.
-      @return an iterator for iterating through this list
-   */
-
-
-
-
-
-   //Class Node
-
-
-   class LinkedListIterator //implements ListIterator
-   {
-	  //private data
-
-
-      /**
+        /**
          Constructs an iterator that points to the front
          of the linked list.
-      */
+         */
 
 
-      /**
+        /**
          Moves the iterator past the next element.
          @return the traversed element
-      */
+         */
 
 
-
-
-
-      /**
+        /**
          Tests if there is an element after the iterator position.
          @return true if there is an element after the iterator position
-      */
+         */
 
 
-      /**
+        /**
          Adds an element before the iterator position
          and moves the iterator past the inserted element.
          @param element the element to add
-      */
+         */
 
 
-
-
-
-
-      /**
+        /**
          Removes the last traversed element. This method may
          only be called after a call to the next() method.
-      */
+         */
 
 
-
-
-
-
-
-      /**
+        /**
          Sets the last traversed element to a different value.
          @param element the element to set
-      */
+         */
 
 
-
-
-   }//LinkedListIterator
+    }//LinkedListIterator
 }//LinkedList
--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -1,32 +1,34 @@
 /**
-   A program that demonstrates the LinkedList class
-*/
+ * A program that demonstrates the LinkedList class
+ */
 public class ListDemo
 {
-   public static void main(String[] args)
-   {
-	  //create LinkedList and add to it
-      LinkedList staff = new LinkedList();
+    public static void main(String[] args)
+    {
+        //create LinkedList and add to it
+        LinkedList staff = new LinkedList();
+        staff.addFirst("Tom");
+        staff.addFirst("Romeo");
+        staff.addFirst("Harry");
+        staff.addFirst("Diana");
 
 
-      // | in the comments indicates the iterator position
+        // | in the comments indicates the iterator position
 
 
-      // Add more elements after second element
+        // Add more elements after second element
 
 
-
-      // Remove last traversed element
-
+        // Remove last traversed element
 
 
-      // Print all elements
+        // Print all elements
 
-      //iterator = staff.listIterator();
-      //while (iterator.hasNext())
-      {
-         //System.out.print(iterator.next() + " ");
-      }
-      //System.out.println();
-   }
+        //iterator = staff.listIterator();
+        //while (iterator.hasNext())
+        {
+            //System.out.print(iterator.next() + " ");
+        }
+        //System.out.println();
+    }
 }
