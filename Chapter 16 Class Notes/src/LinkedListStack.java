24f10710f582526b12676e5fc7f33b1ae49b84fd
Geoff Schmit <gcschmit@gmail.com>
2018-11-02 16:54:43 -0500

    chapter 16 and 17 notes

--- Chapter 16 Class Notes/src/LinkedListStack.java
+++ Chapter 16 Class Notes/src/LinkedListStack.java
@@ -1,52 +1,62 @@
 import java.util.NoSuchElementException;
 
 /**
-   An implementation of a stack as a sequence of nodes.
-*/
-public class LinkedListStack
-{
-   private Node first;
+ * An implementation of a stack as a sequence of nodes.
+ */
+public class LinkedListStack {
+    private Node first;
 
-   /**
-      Constructs an empty stack.
-   */
-   public LinkedListStack()
-   {
-      first = null;
-   }
+    /**
+     * Constructs an empty stack.
+     */
+    public LinkedListStack() {
+        first = null;
+    }
 
-   /**
-      Adds an element to the top of the stack.
-      @param element the element to add
-   */
-
-
-
-
-
-
-   /**
-      Removes the element from the top of the stack.
-      @return the removed element
-   */
+    /**
+     * Adds an element to the top of the stack.
+     *
+     * @param element the element to add
+     */
+    public void push(Object element)
+    {
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = this.first;
+        this.first = newNode;
+    }
 
 
+    /**
+     Removes the element from the top of the stack.
+     @return the removed element
+     */
+    public Object pop()
+    {
+        if(this.empty())
+        {
+            throw new NoSuchElementException();
+        }
+
+        Object element = this.first.data;
+        this.first = this.first.next;
+        return element;
+    }
 
 
+    /**
+     * Checks whether this stack is empty.
+     *
+     * @return true if the stack is empty
+     */
+    public boolean empty()
+    {
+        return (this.first == null);
+    }
 
 
-
-
-   /**
-      Checks whether this stack is empty.
-      @return true if the stack is empty
-   */
-
-
-
-   class Node
-   {
-      public Object data;
-      public Node next;
-   }
+    class Node {
+        public Object data;
+        public Node next;
+    }
 }
